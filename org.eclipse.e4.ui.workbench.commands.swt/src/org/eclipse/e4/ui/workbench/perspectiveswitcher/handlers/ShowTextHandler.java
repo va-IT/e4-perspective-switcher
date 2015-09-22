f03e1100343b2459992b27cf3374935301681dad
thomas.fuerer <tofuatwork@gmail.com>
2015-09-22 09:52:14 +0200

    IN-438: enable ShowTextHandler; if Perspectiveswitcher space is to small refresh of the toolbar need to be done

--- org.eclipse.e4.ui.workbench.commands.swt/src/org/eclipse/e4/ui/workbench/perspectiveswitcher/handlers/ShowTextHandler.java
+++ org.eclipse.e4.ui.workbench.commands.swt/src/org/eclipse/e4/ui/workbench/perspectiveswitcher/handlers/ShowTextHandler.java
@@ -2,13 +2,26 @@ package org.eclipse.e4.ui.workbench.perspectiveswitcher.handlers;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.extensions.Preference;
+import org.eclipse.e4.ui.workbench.perspectiveswitcher.tools.E4PerspectiveSwitcherPreferences;
+import org.osgi.service.prefs.BackingStoreException;
 
 public final class ShowTextHandler {
 	
 	@Execute
-	public void execute()
+	public void execute(@Preference(nodePath=E4PerspectiveSwitcherPreferences.ROOT_PREFERENCES_NODE) IEclipsePreferences prefs)
 					throws InvocationTargetException, InterruptedException {
-		System.out.println("touched");
+	    String currentValue = prefs.get(E4PerspectiveSwitcherPreferences.SHOW_TEXT, Boolean.FALSE.toString());
+	    
+	    prefs.put(E4PerspectiveSwitcherPreferences.SHOW_TEXT, String.valueOf(!Boolean.valueOf(currentValue)));
+		
+	    try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 	}
 }
