f9258d45959c7498526b9a15d9f1afe6696b6717
tofuatjava <tfuerer.javanet@gmail.com>
2016-06-17 18:24:27 +0200

    log exception in ShowTextHandler

--- org.eclipse.e4.ui.workbench.commands.swt/src/org/eclipse/e4/ui/workbench/perspectiveswitcher/handlers/ShowTextHandler.java
+++ org.eclipse.e4.ui.workbench.commands.swt/src/org/eclipse/e4/ui/workbench/perspectiveswitcher/handlers/ShowTextHandler.java
@@ -2,26 +2,30 @@ package org.eclipse.e4.ui.workbench.perspectiveswitcher.handlers;
 
 import java.lang.reflect.InvocationTargetException;
 
+import javax.inject.Inject;
+
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.extensions.Preference;
+import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.e4.ui.workbench.perspectiveswitcher.tools.E4PerspectiveSwitcherPreferences;
 import org.osgi.service.prefs.BackingStoreException;
 
 public final class ShowTextHandler {
-	
-	@Execute
-	public void execute(@Preference(nodePath=E4PerspectiveSwitcherPreferences.ROOT_PREFERENCES_NODE) IEclipsePreferences prefs)
-					throws InvocationTargetException, InterruptedException {
-	    String currentValue = prefs.get(E4PerspectiveSwitcherPreferences.SHOW_TEXT, Boolean.FALSE.toString());
-	    
-	    prefs.put(E4PerspectiveSwitcherPreferences.SHOW_TEXT, String.valueOf(!Boolean.valueOf(currentValue)));
-		
-	    try {
-            prefs.flush();
-        } catch (BackingStoreException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-	}
+  @Inject
+  private Logger log;
+
+  @Execute
+  public void execute(@Preference(nodePath = E4PerspectiveSwitcherPreferences.ROOT_PREFERENCES_NODE) IEclipsePreferences prefs)
+      throws InvocationTargetException, InterruptedException {
+    String currentValue = prefs.get(E4PerspectiveSwitcherPreferences.SHOW_TEXT, Boolean.FALSE.toString());
+
+    prefs.put(E4PerspectiveSwitcherPreferences.SHOW_TEXT, String.valueOf(!Boolean.valueOf(currentValue)));
+
+    try {
+      prefs.flush();
+    } catch (BackingStoreException e) {
+      log.error("Flushing value for property [" + E4PerspectiveSwitcherPreferences.SHOW_TEXT + "] failed.", e);
+    }
+  }
 }
